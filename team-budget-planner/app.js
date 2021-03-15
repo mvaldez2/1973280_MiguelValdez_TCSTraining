395f53fe0b14e6193b8db9efb40afd2b14f372a5
mvaldez2 <miguel.valdez@valpo.edu>
2021-03-15 16:50:30 -0500

    team-budget-planner features and pages created

---
+++ team-budget-planner/app.js
@@ -0,0 +1,34 @@
+// Submits budget plan data to local storage in an array
+function onFormSubmit(){ 
+    var plan = {
+        clientName: document.getElementById("clientName").value,
+        projectName: document.getElementById("projectName").value,
+        budget: document.getElementById("budget").value
+    };
+    var data = JSON.parse(localStorage.getItem('data')) || [];
+    data.push(plan);
+    localStorage.setItem("data", JSON.stringify(data));
+    clearForm();
+}
+function clearForm(){
+    document.getElementById("clientName").value="";
+    document.getElementById("projectName").value="";
+    document.getElementById("budget").value="";
+}
+
+//Parses the json object
+var plans = JSON.parse(localStorage.getItem('data'));
+//Gets tbody element
+var tbody = document.getElementById('data');
+//To calculate total budget
+var totalBudget = 0;
+//Loops through session data to display on table
+for (let index = 0; index < plans.length; index++) {
+    var row = '<tr>';
+    row += '<td>' + plans[index].clientName + '</td>' + '<td>' + plans[index].projectName + '</td>' + '<td>$' + plans[index].budget + '</td>';
+    tbody.innerHTML += row;
+    totalBudget += Number(plans[index].budget) //converts value to number and gets total budget so far
+    
+}
+//To display total budget
+document.getElementById("totalBudget").innerHTML = totalBudget;
